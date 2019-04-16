9502f46ceac34f19e06f11e96c2876767a536be9
Alban Perli <alban.perli@gmail.com>
2019-04-16 11:13:50 +0200

    Network+TableView

---
+++ ImageDownload/ImageDownload/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageDownload
+//
+//  Created by AL on 16/04/2019.
+//  Copyright © 2019 AL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
